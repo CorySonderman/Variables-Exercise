320896bf81b16a868474f3c3a6801d55b21b92ba
CorySonderman <sonderman2000@yahoo.com>
2023-09-28 10:42:15 -0500

    Completed variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Cory";
+            int myAge = 48;
+            char middleInitial = 'M';
+            bool isAlive = true;
+            double myHeight = 72.5;
+            decimal myGrade = 3.99m;
+            string dogName = "Ralph";
+            int dogAge = 10;
+           
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
         }
     }
 }
